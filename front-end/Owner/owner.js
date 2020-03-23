a37a945d35309d3befae24f91f9b2cdcf6376906
Po-Yu Huang <poyu0987@gmail.com>
2020-03-22 22:01:12 -0400

    finish owner front end

--- front-end/Owner/owner.js
+++ front-end/Owner/owner.js
@@ -14,18 +14,20 @@ var apigClient = apigClientFactory.newClient();
 //  messages.scrollTop = messages.scrollHeight;
 //}
 
+var faceId = new URLSearchParams(window.location.search).get("faceId");
+var visitorPhoto;
+
 function loadPhoto(photo) {
     var bucket = photo["bucket"];
     var id = photo["objectKey"];
     var url = "https://" + bucket + ".s3.amazonaws.com/" + id;
-    console.log(url);
-
     document.getElementById("visitorPhoto").src = url;
+    visitorPhoto = [photo];
+    console.log(visitorPhoto);
 }
 
 $(window).on("load", function() {
-    var params = new URLSearchParams(window.location.search);
-    var params = {"faceId": params.get("faceId")};
+    var params = {"faceId": faceId};
     var body = "";
     var additionalParams = "";
 
@@ -34,10 +36,38 @@ $(window).on("load", function() {
         setTimeout(function() {
             loadPhoto(result["data"]["photo"].slice(-1)[0]);
         }, 250);
-
-          
       }).catch( function(result){
         console.log("fail");
       });
-    
+});
+
+$("#owner-decision-form").on("submit", function(e) {
+    var outputArea = $("#panel");
+    var name = $("#name").val();
+    var phoneNumber = $("#phoneNumber").val();
+    e.preventDefault();
+    var message = $("input[name='decision']:checked").val();
+    if (message == "allow") {
+        var params = {};
+        var body = {"faceId": faceId, "is_granted": true, "phoneNumber": phoneNumber, "name": name, "photos": visitorPhoto};
+        console.log(body);
+        var additionalParams = "";
+        apigClient.doorkeyPost(params, body, additionalParams)
+            .then(function(result){
+                setTimeout(function(){
+                    if (result == null){
+                        outputArea.html("<h1> Fail to access database</h1>");
+                    }
+                    else {
+                        console.log(result);
+                        outputArea.html("<h1> Allow access </h1>");
+                    }
+                }, 250);
+            }).catch(function(result){
+                outputArea.html("<h1> Fail to access database</h1>");
+            });
+    }
+    else {
+        outputArea.html("<h1> Deny access </h1>");
+    }
 });
